2d7f24bc97e9e5ca064bbaf7585a154435b66d65
TheDripper <tylerehill@gmail.com>
2018-07-26 19:40:21 -0700

    start

---
+++ pages/index.vue
@@ -0,0 +1,68 @@
+<template>
+<div class=work>
+	<video playsinline autoplay muted loop id=splash src=back.mp4></video>
+	<h1 id=flash class=day>drownlux</h1>
+</div>
+</template>
+<script>
+
+</script>
+<style>
+@keyframes blink {
+  50% {
+    opacity: 0.0; } }
+
+@-webkit-keyframes blink {
+  50% {
+    opacity: 0.0; } }
+
+.blink {
+  animation: blink 1s step-start 0s infinite;
+  -webkit-animation: blink 1s step-start 0s infinite; }
+
+@keyframes zoom {
+  from {
+    transform: translate(-50%, -50%) scale(1); }
+  to {
+    transform: translate(-50%, -10%) scale(2); } }
+
+@font-face {
+  font-family: "vcr";
+  src: url("~/assets/vcr.ttf"); }
+
+html, body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: blue; }
+
+video {
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  min-width: 100%;
+  min-height: 100%;
+  transform: translate(-50%, -10%) scale(2);
+  animation-duration: 10s;
+  animation-name: zoom; }
+
+h1 {
+  font-family: "vcr";
+  font-size: 64px;
+  text-transform: uppercase;
+  position: relative;
+  z-index: 999; }
+
+.day {
+  color: white;
+  cursor: pointer;
+  text-shadow: 5px 5px 0 black, -5px -5px 0 black, -5px 5px 0 black, 5px -5px 0 black; }
+
+.night {
+  color: lime;
+  cursor: pointer; }
+</style>
