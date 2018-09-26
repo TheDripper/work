c68dfe57d969137253a49316c27d16249d06adad
TheDripper <tylerehill@gmail.com>
2018-09-26 13:13:02 -0700

    news 1.0

--- pages/news.vue
+++ pages/news.vue
@@ -1,35 +1,52 @@
 <template>
-<div>
-<li v-for="hit in hits" @click="getHit"> 
-<h1>
-<span v-for="(word, index) in hit.title">{{ word }} </span>
-</h1>
-</li>
+<div id=main>
+<div id=left>
+	<li v-for="hit in hits" @click="getHit"> 
+	<h1 :data-link="hit.link">
+	<span v-for="(word, index) in hit.title" v-bind:class="{ trio: index<3 }">{{ word }} </span>
+	</h1>
+	</li>
+</div>
+<div id=right>
+	<p v-for="graph in graphs">{{ graph.text }}</p>
+</div>
 </div>
 </template>
 <script>
 let axios = require('axios')
+const cheerio = require('cheerio')
 export default {
 	async asyncData(context) {
 		try {
 			let hits = []
+			let graphs = [{text:''}]
 			let { data } = await axios('https://www.reddit.com/r/worldnews.json?limit=50')
 			data.data.children.forEach(post=>{
 				let hitter = {
 					title: post.data.title.split(' '),
-					link: 'https://www.reddit.com'+post.data.permalink
+					link: post.data.url,
+					id: post.data.id
 				}
-				console.log(hitter)
 				hits.push(hitter)
 			})
-			return { hits: hits }
+			return { hits: hits, graphs: graphs }
 		} catch(err) {
 			console.log(err)
 		}
 	},
 	methods: {
-		getHit: e=>{
-			e.target.style.color = "blue"
+		getHit: async function(e) {
+			document.querySelector('#left').classList.add('freeze')
+			document.querySelector('#right').classList.add('freeze')
+			let link = e.target.parentNode.dataset.link
+			e.target.parentNode.parentNode.classList.add('open')
+			console.log(link)
+			let graphs = await axios.post('/hit',{hit:link})
+			console.log(graphs)
+			this.graphs = graphs.data.text
+			this.mages = graphs.data.mages
+			document.querySelector('#left').style.width = '17vw'
+			document.querySelector('#right').style.width = '83vw'
 		}
 	}
 }
@@ -40,11 +57,15 @@ html, body {
 	padding: 0;
 	background: #F2EBCD;
 	font-family: "Times", Times, serif;
+	box-sizing: border-box;
 }
 li {
 	list-style: none;
 	padding: 20px 0;
 	position: relative;
+	cursor: pointer;
+}
+li:hover {
 }
 li a {
 	position: absolute;
@@ -53,9 +74,60 @@ li a {
 	top: 0;
 	left: 0;
 }
-h1 span:nth-child(1), h1 span:nth-child(2), h1 span:nth-child(3) {
+h1 .trio {
 	text-transform: uppercase;
 }
 
-
+#main {
+	display: flex;
+}
+#right {
+	width: 10vw;
+	transition: all 0.5s ease;
+}
+#left {
+	transition: all 0.5s ease;
+	width: 90vw;
+}
+.freeze {
+	height: 100vh;
+	overflow: scroll;
+}
+.freeze li:not(.open) {
+	display: none;
+}
+.open {
+	padding: 0;
+}
+.open span:not(.trio) {
+	display: none;
+}
+.open h1 {
+	height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-around;
+	align-items: center;
+	font-size: 11vw;
+	word-break: break-all;
+}
+#right p {
+	font-size: 24px;
+	display: inline;
+}
+#right p:nth-child(3n) {
+	text-transform: uppercase;
+	font-weight: bold;
+	font-size: 28px;
+	padding: 0;
+}
+#right p:nth-child(4n) {
+	font-style: italic;
+}
+#right p:nth-child(5n) {
+	background: black;
+	color: white;
+	font-family: "courier";
+	padding: 0;
+}
 </style>
--- server/index.js
+++ server/index.js
@@ -4,12 +4,19 @@ const { Nuxt, Builder } = require('nuxt')
 const app = express()
 const host = process.env.HOST || '127.0.0.1'
 const port = process.env.PORT || 3000
+const bodyParser = require('body-parser')
+const axios = require('axios')
+const cheerio = require('cheerio')
 
+app.use(bodyParser.json())
 app.set('port', port)
 
 // Import and Set Nuxt.js options
 let config = require('../nuxt.config.js')
 config.dev = !(process.env.NODE_ENV === 'production')
+
+
+
 
 async function start() {
   // Init Nuxt.js
@@ -20,9 +27,28 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+  
+  app.post('/hit',async function(req,res,next){
+        let { data } = await axios(req.body.hit)
+        let text = []
+	let mages = []
+        const $ = cheerio.load(data)
+        $('p').each(function(index,elem){
+        	text.push({text:$(this).text(),id:'graph'+index})
+        })
+	$('img').each(function(){
+		console.log($(this).attr('src'))
+	})
+	let sender = {
+		text: text,
+		mages: mages
+	}
+	res.send(sender)
+  })
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
+  
 
   // Listen the server
   app.listen(port, host)
