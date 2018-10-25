21c929afec716eb73d72213ba6d20760f5460b82
TheDripper <tylerehill@gmail.com>
2018-10-25 15:32:09 -0700

    HOT TAPE

--- pages/colors.vue
+++ pages/colors.vue
@@ -1,47 +1,83 @@
 <template> 
 <div id=blog>
-<div id=mages>
+<div class=mages id=one>
 <ul>
-	<li id=blu v-for="hit in blue" v-bind:style="{ backgroundImage:'url('+hit.webformatURL+')' }">
+	<span class=after id=blucol><a href=/posts/1>One</a></span>
+	<li id=blu v-for="hit in hits.blue" v-bind:style="{ backgroundImage:'url('+hit+')' }">
+	</li>
+
+</ul>
+<ul>
+	<span class=after id=cycol><a href=/posts/2>Two</a></span>
+	<li id=tur v-for="hit in hits.turquoise" v-bind:style="{ backgroundImage:'url('+hit+')' }">
 	</li>
 </ul>
 <ul>
-	<li id=tur v-for="hit in turq" v-bind:style="{ backgroundImage:'url('+hit.webformatURL+')' }">
+	<span class=after id=purpcol><a href=/posts/3>Three</a></span>
+	<li id=lac v-for="hit in hits.lilac" v-bind:style="{ backgroundImage:'url('+hit+')' }">
 	</li>
 </ul>
 <ul>
-	<li id=lac v-for="hit in lac" v-bind:style="{ backgroundImage:'url('+hit.webformatURL+')' }">
+	<span class=after id=blackcol><a href=/posts/4>Four</a></span>
+	<li id=noir v-for="hit in hits.black" v-bind:style="{ backgroundImage:'url('+hit+')' }">
+	</li>
+</ul>
+<h1 id=insta>INSTA</h1>
+<h1 id=hot>HOT TAPE</h1>
+</div>
+<div class=mages id=two>
+<ul>
+	<span class=after id=redcol></span>
+	<li id=blu v-for="hit in hits.red" v-bind:style="{ backgroundImage:'url('+hit+')' }">
 	</li>
 </ul>
 <ul>
-	<li id=noir v-for="hit in black" v-bind:style="{ backgroundImage:'url('+hit.webformatURL+')' }">
+	<span class=after id=orangecol></span>
+	<li id=tur v-for="hit in hits.orange" v-bind:style="{ backgroundImage:'url('+hit+')' }">
 	</li>
 </ul>
-<h1 id=start>OUR STORY BEGINS</h1>
+<ul>
+	<span class=after id=pinkcol></span>
+	<li id=lac v-for="hit in hits.pink" v-bind:style="{ backgroundImage:'url('+hit+')' }">
+	</li>
+</ul>
+<ul>
+	<span class=after id=graycol></span>
+	<li id=noir v-for="hit in hits.gray" v-bind:style="{ backgroundImage:'url('+hit+')' }">
+	</li>
+</ul>
 </div>
 </div>
 </template>
 <script>
 let axios = require('axios')
+let getColor = async function(color) {
+	console.log(color)
+	let pics = await axios('https://pixabay.com/api/?key=10493578-e0c15b403dc08f3bc106b40b8&per_page=12&colors='+color)
+	let urls = []
+	pics.data.hits.forEach(hit=>{
+		urls.push(hit.previewURL)
+	})
+	return urls
+}
 export default {
 	async asyncData(context) {
-		try {
-			let  blue = await axios('https://pixabay.com/api/?key=10493578-e0c15b403dc08f3bc106b40b8&colors=blue')
-			blue = blue.data.hits
-			let  turq = await axios('https://pixabay.com/api/?key=10493578-e0c15b403dc08f3bc106b40b8&colors=turquoise')
-			turq = turq.data.hits
-			let  lac = await axios('https://pixabay.com/api/?key=10493578-e0c15b403dc08f3bc106b40b8&colors=lilac')
-			lac = lac.data.hits
-			let  black = await axios('https://pixabay.com/api/?key=10493578-e0c15b403dc08f3bc106b40b8&colors=black')
-			black = black.data.hits
-			return {
-				blue: blue,
-				turq: turq,
-				lac: lac,
-				black: black
-			}
-		} catch(err) {
-			console.log(err)
+		let colors = [
+			'blue',
+			'turquoise',
+			'lilac',
+			'black',
+			'red',
+			'orange',
+			'pink',
+			'gray'
+		]
+		let hits = {}
+		for(const color of colors) {
+			hits[color] = await getColor(color)
+		}
+		return {
+			hits: hits
 		}
 	}
 }
@@ -52,46 +88,223 @@ export default {
 }
 html, body {
 	margin: 0;
-}
-#mages {
-	transform: rotate(-90deg);
-	height: 100vw;
+	background: #F2EBCD;
 }
 ul {
 	list-style: none;
 	padding: 0;
-	display: flex;
-	transform: rotate(-20deg) translateX(-250vw);
-	transform-origin: right top;
-	height: 25vw;
+	width: 100px;
+	height: 100vh;
+	position: relative;
+	transform: rotate(-10deg) translateX(50vw);
 }
-li {
-	width: 25vw;
-	height: 25vw;
-	background-size: cover;
-	flex-shrink: 0;
-	transform: rotate(90deg);
-	transform-origin: right top;
-
+#two {
 }
-#blu {
+#two ul {
+	transform: rotate(10deg) translate(50vw,-40vh);
+}
+ul:nth-child(1) {
 	background: blue;
 }
-#start {
+#one ul:nth-child(1):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: blue;
 	position: absolute;
-	bottom: 9vw;
-	right: 0px;
-	font-size: 64px;
-	transform: rotate(160deg);
+	top: 0;
+	transform: rotate(180deg);
 }
-@media(max-width:1050px) {
-	#start {
-		font-size: 32px;
+#one ul:nth-child(2):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: teal;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#one ul:nth-child(3):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: purple;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#one ul:nth-child(4):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: black;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#two ul:nth-child(1):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: red;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#two ul:nth-child(2):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: orange;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#two ul:nth-child(3):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: pink;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+#two ul:nth-child(4):after {
+	content: '';
+	width: 100px;
+	height: 230vh;
+	background: black;
+	position: absolute;
+	top: 0;
+	transform: rotate(180deg);
+}
+ul:nth-child(2) {
+	background: teal;
+}
+ul:nth-child(3) {
+	background: purple;
+}
+ul:nth-child(4) {
+	background: black; 
+}
+
+#two ul:nth-child(1) {
+	background: red;
+}
+#two ul:nth-child(2) {
+	background: brown;
+}
+#two ul:nth-child(3) {
+	background: pink;
+}
+#two ul:nth-child(4) {
+	background: black; 
+}
+.after {
+	width: 100px;
+	height: 230vh;
+	position: absolute;
+	margin-top: -20vh;
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+}
+.after a {
+	text-decoration: none;
+	font-size: 48px;
+	font-weight: bold;
+	transform: rotate(10deg);
+	margin-top: 50vw;
+}
+#blucol {
+	background: blue;
+	transform: translateX(-100px);
+}
+#blucol a {
+	color: cyan;
+}
+#cycol {
+	background: cyan;
+	transform: translateX(-299px);
+}
+#purpcol a {
+	color: black;
+}
+#purpcol {
+	background: purple;
+	transform: translateX(-498px);
+}
+#blackcol {
+	background: black; 
+	transform: translateX(-696px);
+}
+#blackcol a {
+	color: white;
+}
+
+#redcol {
+	background: red;
+	transform: translateX(-100px);
+}
+#orangecol {
+	background: orange;
+	transform: translateX(-299px);
+}
+#pinkcol {
+	background: pink;
+	transform: translateX(-498px);
+}
+#graycol {
+	background: black; 
+	transform: translateX(-696px);
+}
+
+li {
+	background-size: cover;
+	flex-shrink: 0;
+	width: 100px;
+	height: 100px;
+	position: relative;
+	z-index: 5;
+}
+#blog {
+	width: 100vw;
+	height: 100vh;
+}
+.mages {
+	display: flex;
+	height: 100vh;
+	overflow: hidden;
+	position: relative;
+}
+#insta {
+	position: absolute;
+	top: 20px;
+	right: 10%;
+	transform: rotate(-8deg);
+	font-size: 48px;
+	color: black;
+}
+#hot {
+	position: absolute;
+	bottom: 20px;
+	left: 20px;
+	font-size: 48px;
+	color: black;
+}
+@media(max-width:1200px) {
+	#insta {
+		transform: rotate(-8deg) translateX(-100%);
+		color: white;
 	}
 }
-@media(max-width:532px) {
-	#start {
-		font-size: 18px;
+@media(max-width:430px) {
+	ul {
+		transform: rotate(-10deg) translateY(-5vw);
 	}
+	#two ul {
+		transform: rotate(10deg) translateY(-5vw);
+	}
+
 }
 </style>
---
+++ pages/posts/_id.vue
@@ -0,0 +1,63 @@
+<template>
+<div id=post>
+<div id=top>
+<span id=black class=bar></span>
+<span id=purple class=bar></span>
+<span id=turq class=bar></span>
+<span id=blu class=bar></span>
+</div>
+
+<h1>Post: {{ id }}</h1>
+<h2>Content Here</h2>
+<a href=/colors/>Back to archive <<< </a>
+
+<div id=bottom>
+<span id=red class=bar></span>
+<span id=orange class=bar></span>
+<span id=pink class=bar></span>
+<span id=black class=bar></span>
+</div>
+
+</div>
+</template>
+<script>
+export default {
+	asyncData(context) {
+		return {
+			id: context.params.id
+		}
+	}
+}
+</script>
+<style>
+#black { background: black; }
+#blu { background: blue; }
+#turq { background: cyan; }
+#purple { background: purple; }
+#red { background: red; }
+#orange { background: orange; }
+#pink { background: pink; }
+.bar {
+	width: 100vw;
+	height: 50px;
+	display: block;
+}
+#top {
+	position: fixed;
+	top: 0;
+	left: 0;
+}
+#bottom {
+	position: fixed;
+	bottom: 0;
+	left: 0;
+}
+#post {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 100vw;
+	height: 100vh;
+}
+</style>
